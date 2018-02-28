face8eba10f0f4550789ac2a885e6d856d2a6a87
uu.karachevcev <uu.karachevcev@tensor.ru>
2018-02-28 15:48:24 +0300

    изменил набор данных класса Ad

--- AdDiary/src/app/adlist.service.ts
+++ AdDiary/src/app/adlist.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import {Ad, User } from './fl';
 
 @Injectable()
 export class AdlistService {
-
+   AdList: Ad[];
   constructor() { }
+  getDemoData(): Ad[] {
+     this.AdList = [];
+     this.AdList.push(new Ad(130, 90, 95, 140, 90, 107,'', ''));
+     this.AdList.push(new Ad(126, 90, 84, 140, 90, 107,'', ''));
+     return this.AdList;
+  }
 
 }
--- AdDiary/src/app/fl.ts
+++ AdDiary/src/app/fl.ts
@@ -1,4 +1,4 @@
-class User {
+export class User {
    id: number;
    name: string;
 
@@ -12,19 +12,25 @@ class User {
    }
 }
 
-class Ad {
+export class Ad {
    id: number;
    date: Date;
-   morning: number;
-   evening: number;
-   pulse: number;
+   morning1: number;
+   morning2: number;
+   morningPulse: number;
+   evening1: number;
+   evening2: number;
+   eveningPulse: number;
    description: string;
    meds: string;
 
-   constructor(morning, evening, pulse, description, meds) {
-      this.morning = morning;
-      this.evening = evening;
-      this.pulse = pulse;
+   constructor(morning1, morning2, morningPulse, evening1, evening2, eveningPulse, description, meds) {
+      this.morning1 = morning1;
+      this.morning2 = morning2;
+      this.morningPulse = morningPulse;
+      this.evening1 = evening1;
+      this.evening2 = evening2;
+      this.eveningPulse = eveningPulse;
       this.description = description;
       this.meds = meds;
    }
