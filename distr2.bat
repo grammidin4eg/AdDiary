dd402d8050a143069995c7c4435e51d1c3549976
region/uu.karachevcev <grammidin4eg@gmail.com>
2018-03-17 13:12:58 +0300

    loginza

---
+++ distr2.bat
@@ -0,0 +1,6 @@
+set distr="D:\pro\AdDiary\AdDiary\dist"
+set bldir="D:\pro\simbo"
+copy loginza_55818d9204bf54c59fe29f85679441c6.html %distr%
+copy dbconf.php %distr%
+copy %bldir%\simbo.php %distr%
+copy %bldir%\adDiary.php %distr%
