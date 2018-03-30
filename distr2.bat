14c719eef4fce0a8948614ae257a6e41e39fe6c7
region/uu.karachevcev <grammidin4eg@gmail.com>
2018-03-30 07:37:17 +0300

    bat

--- distr2.bat
+++ distr2.bat
@@ -1,8 +1,11 @@
 cd /d D:\pro\AdDiary\AdDiary
 ng build --prod
+pause
 set distr="D:\pro\AdDiary\AdDiary\dist"
 set bldir="D:\pro\simbo"
-copy dbconf.php %distr%
+copy D:\pro\AdDiary\dbconf.php %distr%
 copy %bldir%\simbo.php %distr%
 copy %bldir%\adDiary.php %distr%
 copy %bldir%\user.php %distr%
+"C:\Program Files\7-Zip\7z.exe" a distr.zip %distr%\*.* -r
+pause
