aa11faf7efaa3864e6f05cbbf204514084be392b
Karachevtsev Yu Yu <grammidin4eg@gmail.com>
2019-02-10 01:19:48 +0300

    подготовка

--- addiary/src/components/app/App.js
+++ addiary/src/components/app/App.js
@@ -4,7 +4,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">        
+      <div className="App">      
+           
       </div>
     );
   }
