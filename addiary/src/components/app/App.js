5cd027586e3e0196e5b074c6605c6d10ef34edfc
Karachevtsev Yu Yu <grammidin4eg@gmail.com>
2019-02-10 19:52:33 +0300

    лендинг

--- addiary/src/components/app/App.js
+++ addiary/src/components/app/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Landing from '../landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">      
-           
+          <Landing></Landing>
       </div>
     );
   }
