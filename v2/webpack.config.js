5266076be9023777ad3bd7a2d18a3c7aaf5fcdbd
uu.karachevcev <uu.karachevcev@tensor.ru>
2019-04-02 16:25:47 +0300

    babel and webpack

---
+++ v2/webpack.config.js
@@ -0,0 +1,21 @@
+// Для использования нужен Node.JS
+// Поставьте webpack:
+//   npm i -g webpack
+// Поставьте babel-loader:
+//   npm i babel-loader
+// Запустите его в директории с файлами:
+//   webpack
+
+module.exports = {
+    entry: './src/addiary.js',
+
+    output: {
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            { test: /\.js$/, loader: "babel-loader" }
+        ]
+    }
+};
