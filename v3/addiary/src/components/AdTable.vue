d9f9e2d965252480994cd621085ec621cb0cc6c9
uu.karachevcev <uu.karachevcev@tensor.ru>
2019-04-26 19:18:53 +0300

    comment && meds

--- v3/addiary/src/components/AdTable.vue
+++ v3/addiary/src/components/AdTable.vue
@@ -1,43 +1,43 @@
 <template>
-  <div class="container-fluid adtable">
+<div class="container-fluid adtable">
     <div v-if="regError" class="alert alert-danger" role="alert">{{ regError }}</div>
-    <editor v-if="showEditor" :itemProp="curEditItem" @endEdit="editorEndEdit"/>
+    <editor v-if="showEditor" :itemProp="curEditItem" @endEdit="editorEndEdit" />
     <table class="table table-striped table-bordered">
-      <thead>
-        <tr>
-          <th scope="col"></th>
-          <th scope="col" colspan="3">Утро</th>
-          <th scope="col" colspan="3">Вечер</th>
-          <th scope="col"></th>
-          <th scope="col"></th>
-        </tr>
-        <tr>
-          <th scope="col">Дата</th>
-          <th scope="col">sys</th>
-          <th scope="col">dia</th>
-          <th scope="col">пульс</th>
-          <th scope="col">sys</th>
-          <th scope="col">dia</th>
-          <th scope="col">пульс</th>
-          <th scope="col">Состояние</th>
-          <th scope="col">Лекарства</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in items" :key="item.id" class="table-row" @click="onItemClick(item)">
-          <th scope="row">{{ item.date }}</th>
-          <td>{{ item.period1.sys }}</td>
-          <td>{{ item.period1.dia }}</td>
-          <td>{{ item.period1.pulse }}</td>
-          <td>{{ item.period2.sys }}</td>
-          <td>{{ item.period2.dia }}</td>
-          <td>{{ item.period2.pulse }}</td>
-          <td>{{ item.comment }}</td>
-          <td>{{ item.med }}</td>
-        </tr>
-      </tbody>
+        <thead>
+            <tr>
+                <th scope="col"></th>
+                <th scope="col" colspan="3">Утро</th>
+                <th scope="col" colspan="3">Вечер</th>
+                <th class="text-field" scope="col"></th>
+                <th class="text-field" scope="col"></th>
+            </tr>
+            <tr>
+                <th scope="col">Дата</th>
+                <th scope="col">sys</th>
+                <th scope="col">dia</th>
+                <th scope="col">пульс</th>
+                <th scope="col">sys</th>
+                <th scope="col">dia</th>
+                <th scope="col">пульс</th>
+                <th class="text-field" scope="col">Комментарий</th>
+                <th class="text-field" scope="col">Лекарства</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in items" :key="item.id" class="table-row" @click="onItemClick(item)">
+                <th scope="row">{{ item.date }}</th>
+                <td>{{ item.period1.sys }}</td>
+                <td>{{ item.period1.dia }}</td>
+                <td>{{ item.period1.pulse }}</td>
+                <td>{{ item.period2.sys }}</td>
+                <td>{{ item.period2.dia }}</td>
+                <td>{{ item.period2.pulse }}</td>
+                <td class="text-field">{{ item.comment }}</td>
+                <td class="text-field">{{ item.med }}</td>
+            </tr>
+        </tbody>
     </table>
-  </div>
+</div>
 </template>
 
 <script>
@@ -47,68 +47,86 @@ import Editor from './Editor';
 const adDiaryService = new AdDiaryService();
 
 export default {
-  name: "AdTable",
-  props: {
-    user: Object
-  },
-  data() {
-    return {
-      regError: null,
-      itemsData: null,
-      curEditItem: {},
-      showEditor: false
-    };
-  },
-  computed: {
-    items() {      
-      if (this.user) {
-        if (this.itemsData) {
-          return this.itemsData;
-        } else {
-          this.loadTable();
-          return null;
+    name: "AdTable",
+    props: {
+        user: Object
+    },
+    data() {
+        return {
+            regError: null,
+            itemsData: null,
+            curEditItem: {},
+            showEditor: false
+        };
+    },
+    computed: {
+        items() {
+            if (this.user) {
+                if (this.itemsData) {
+                    return this.itemsData;
+                } else {
+                    this.loadTable();
+                    return null;
+                }
+            }
+
+            return null;
         }
-      } 
-
-      return null;      
-    }
-  },
-  methods: {
-    loadTable() {
-      adDiaryService.list(this.user.id).then(result => {
-        console.log("result list", result);
-         this.itemsData = result;
-      }, this.showError);
     },
+    methods: {
+        loadTable() {
+            adDiaryService.list(this.user.id).then(result => {
+                console.log("result list", result);
+                this.itemsData = result;
+            }, this.showError);
+        },
 
-    onItemClick(item) {
-      console.log('item click', item);
-      this.curEditItem = item;
-      this.showEditor = true;
+        onItemClick(item) {
+            console.log('item click', item);
+            this.curEditItem = item;
+            this.showEditor = true;
+            if (this.$children.length) {
+                this.$children[0].loadAnotherItem(item);
+            }
+            //debugger;
+        },
+
+        editorEndEdit() {
+            this.showEditor = false;
+        }
     },
-
-    editorEndEdit() {
-      this.showEditor = false;
+    components: {
+        Editor
     }
-  },
-  components: {
-    Editor
-  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-th, td {
-  text-align: center;
+th,
+td {
+    text-align: center;
 }
 
 tr.table-row:hover {
-  background-color: blanchedalmond;
-  cursor: pointer;
+    background-color: blanchedalmond;
+    cursor: pointer;
 }
 
 .adtable {
-  flex-grow: 1;
+    flex-grow: 1;
+}
+
+.text-field {
+    max-width: 180px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+
+@media screen and (max-width: 700px) {
+  .text-field {
+    display: none;
+  }
 }
 </style>
--- v3/addiary/src/components/Editor.vue
+++ v3/addiary/src/components/Editor.vue
@@ -1,32 +1,51 @@
 <template>
-    <div class="editor">
-        <div class="first">
-            <div class="date">{{item.date}}</div>
-            <div class="resizer"></div>
-            <div class="period-buttons">
-                <button type="button" class="btn" :class="butonDayClass" @click="switchBtn(true)">Утро</button>
-                <button type="button" class="btn" :class="butonNightClass" @click="switchBtn(false)">Вечер</button>
-            </div>
-        </div>
-        <div class="ad">
-            <input                                
-                type="text"
-                class="form-control"
-                id="regEmail"
-                placeholder="sys/dis/пульс"
-                v-model="inputModel"
-                v-on:focus="activatedEdit()"
-              >
-        </div>
-        <div class="meds"></div>
-        <div class="comment"></div>
-        <div class="func-button">
-            <button type="button" class="btn btn-primary">Сохранить</button>
-            <button type="button" class="btn btn-primary" @click="close">Закрыть</button>
-            <button type="button" class="btn btn-primary">Удалить</button>
+<div class="editor">
+    <div class="first">
+        <div class="date">{{item.date}}</div>
+        <div class="resizer"></div>
+        <div class="period-buttons">
+            <button type="button" class="btn" :class="butonDayClass" @click="switchBtn(true)">Утро</button>
+            <button type="button" class="btn" :class="butonNightClass" @click="switchBtn(false)">Вечер</button>
         </div>
     </div>
+    <div class="ad">
+        <input
+        type="text"
+        class="form-control"
+        id="adEdit"
+        placeholder="sys/dis/пульс"
+        v-model="inputModel"
+        v-on:focus="activatedEdit()"
+      />
+    </div>
+        <div class="meds">
+            <label for="adMeds">Лекарства</label>
+            <input
+                type="text"
+                class="form-control"
+                id="adMeds"
+                placeholder="Лекарства"
+                v-model.trim="item.med"
+            />
+        </div>
+        <div class="comment">
+            <label for="adComment">Комментарий</label>
+            <input
+                type="text"
+                class="form-control"
+                id="adComment"
+                placeholder="Комментарий. Например, самочувствие."
+                v-model.trim="item.comment"
+            />
+    </div>
+            <div class="func-button">
+                <button type="button" class="btn btn-primary">Сохранить</button>
+                <button type="button" class="btn btn-primary" @click="close">Закрыть</button>
+                <button type="button" class="btn btn-primary">Удалить</button>
+            </div>
+        </div>
 </template>
+
 <script>
 export default {
     name: "editor",
@@ -40,26 +59,67 @@ export default {
             objDay: this.itemProp.period1,
             objEvening: this.itemProp.period2,
             inputObj: this.itemProp.period1,
-            inputModel: this.getObjText(this.itemProp.period1)
-        }
+            inputModel: this.getObjText(this.itemProp.period1),
+            lastInputModel: null
+        };
     },
     beforeUpdate() {
-        console.log('beforeUpdate', this.inputObj, this.inputModel);
-        /*if (!this.firstLoad) {
-            this.firstLoad = false;
-            if (this.inputObj.sys) {
-                this.inputModel = `${this.inputObj.sys}/${this.inputObj.dia}/${this.inputObj.pulse}`;
+        console.log("beforeUpdate obj:", this.inputObj, "model:", this.inputModel);
+        if (this.lastInputModel !== this.inputModel) {            
+            const valueList = this.inputModel.split("/");
+            let curIndex = valueList.length - 1;
+            let value = parseInt(valueList[curIndex], 10);
+            if (isNaN(value)) {
+                value = 0;
             }
-        }*/
-    },    
+            if (value > 400) {
+                value = valueList[curIndex].charAt(valueList[curIndex].length - 1);
+                curIndex++;
+            }
+            switch (curIndex) {
+                case 0:
+                    this.inputObj.sys = value;
+                    break;
+                case 1:
+                    this.inputObj.dia = value;
+                    break;
+                case 2:
+                    this.inputObj.pulse = value;
+                    break;
+            }
+            let res =
+                this.inputObj.sys && !isNaN(this.inputObj.sys) ?
+                "" + this.inputObj.sys :
+                "";
+            if (this.inputObj.dia && !isNaN(this.inputObj.dia)) {
+                res += "/" + this.inputObj.dia;
+            }
+            if (this.inputObj.pulse && !isNaN(this.inputObj.pulse)) {
+                res += "/" + this.inputObj.pulse;
+            }
+            const nextValue = value * 10;
+            if (curIndex < 2 && nextValue > 400) {
+                res += "/";
+            }
+            this.inputModel = res;
+            this.lastInputModel = res;
+        }
+
+        if (this.item.comment && (this.item.comment.length > 100)) {
+            this.item.comment = this.item.comment.substr(0, 100);
+        }
+    },
 
     methods: {
         close() {
-            this.$emit('endEdit');
+            this.$emit("endEdit");
         },
 
-        switchBtn(val) {            
-            this.switcherDay = val;                   
+        switchBtn(val) {
+            this.switcherDay = val;
+            this.inputObj = val ? this.objDay : this.objEvening;
+            this.inputModel = this.getObjText(this.inputObj);
+            console.log('switch', val, this.getObjText(this.inputObj));
         },
 
         getObjText(obj) {
@@ -67,25 +127,41 @@ export default {
         },
 
         activatedEdit(event) {
-            console.log('activated');
+            console.log("activated");
+            this.inputModel = "";
+            this.inputObj.sys = 0;
+            this.inputObj.dia = 0;
+            this.inputObj.pulse = 0;
             //this.inputModel = this.getObjText(this.inputObj);
+        },
+
+        loadAnotherItem(item) {
+            this.item = item;
+            this.switcherDay = true;
+            this.objDay = item.period1;
+            this.objEvening = item.period2;
+            this.inputObj = item.period1;
+            this.inputModel = this.getObjText(item.period1);
+            this.lastInputModel = null;
         }
     },
     computed: {
         butonDayClass() {
-            return (this.switcherDay) ? 'btn-success' : 'btn-light';
+            return this.switcherDay ? "btn-success" : "btn-light";
         },
 
         butonNightClass() {
-            return (this.switcherDay) ? 'btn-light' : 'btn-success';
+            return this.switcherDay ? "btn-light" : "btn-success";
         }
     }
-}
+};
 </script>
+
 <style scoped>
 .editor {
     position: absolute;
-    height: 180px;
+    height: 315px;
+    min-width: 312px;    
     width: 50%;
     background-color: aliceblue;
     margin: auto;
@@ -95,7 +171,7 @@ export default {
     right: 0;
     border: solid 2px #908b8b;
     border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0,0,0,0.5);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
     padding: 12px;
 }
 
@@ -116,6 +192,9 @@ button {
 .func-button {
     float: right;
 }
+
+.meds,
+.comment {
+    margin: 8px 0;
+}
 </style>
-
-
