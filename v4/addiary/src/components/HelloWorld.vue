7fbff0382c0ce050de16bbffcf00284bbfc17d3b
uu.karachevcev <uu.karachevcev@tensor.ru>
2019-08-02 17:46:43 +0300

    v4

---
+++ v4/addiary/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+       <router-link to="/reg">Registration</router-link>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+
+  })
+};
+</script>
